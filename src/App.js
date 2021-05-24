0dc92968007ed1917845a8338a872c81b007cc7d
Barsha96 <barsha.upadhaya1996@gmail.com>
2021-05-25 00:35:43 +0545

    initial setup tailwind-emtion react-router-dom craco

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, { Suspense} from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Root from './components/Root';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+		// 	<Switch>
+		// 		<Suspense fallback={<LoadingContainer text="loading ..." />}>
+		// 			<Route path="/" exact component={Home} />
+		// 			<Route path="/register" exact component={Register} />
+		// 			<Route path="/login" exact component={Login} />
+		// 			<Route path="/reset-password" exact component={ResetPassword} />
+		// 			<Route path="/force-password-change" exact component={ForceResetPassword} />
+		// 			<Route path="/create-your-team" exact component={CreateYourFirstTeam} />
+		// 			<Route exact path="/verify-email" component={VerifyEmail} />
+		// 		</Suspense>
+		// 		<Route component={NotFound} />
+		// 	</Switch>
+		// </Router>
+    <Root/>
   );
 }
 
---
+++ src/components/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import tw from 'twin.macro';
+
+const Root = () => {
+    return (
+        <div css={tw`bg-red-100`}>
+            This is my canvas
+        </div>
+    )
+}
+
+export default Root
